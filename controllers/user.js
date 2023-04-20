2cc58877b55b702bce4e2fc80a8a1ecaf5edd085
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-04-20 17:06:45 +0530

    second commit user

--- controllers/user.js
+++ controllers/user.js
@@ -1,48 +1,39 @@
 import { User } from "../models/user.js";
 
+//register new user
 export const register = async (req, res) => {
-    try {
-      const { name, email, phone,website } = req.body;
-  
-    //   let user = await User.findOne({ email });
-  
-    //   if (user) return next(new ErrorHandler("User Already Exist", 400));
-  
-    //   const hashedPassword = await bcrypt.hash(password, 10);
-  
-     const user = await User.create({ name, email, phone , website });
-  
-    //   sendCookie(user, res, "Registered Successfully", 201);
-    res.status(200).json({
-      success: true,
-      message:"done",
-    });
-    } catch (error) {
-      console.log(error)
-    }
-  };
-
-  export const getAlluser = async (req, res, next) => {
   try {
-   
-
-    const users = await User.find();
-
+    const { name, email, phone, website } = req.body;
+    const user = await User.create({ name, email, phone, website });
     res.status(200).json({
       success: true,
-      users,
+      message: "done",
     });
   } catch (error) {
-   
+    console.log(error);
   }
 };
 
+
+
+//get all the registerd user
+export const getAlluser = async (req, res, next) => {
+  try {
+    const users = await User.find();
+
+    res( users)
+  
+     
+    
+  } catch (error) {}
+};
+
+//update user
 export const updateUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
     const { name, email, phone, website } = req.body;
-   
-    
+
     // update the fields
     user.name = name;
     user.email = email;
@@ -56,26 +47,24 @@ export const updateUser = async (req, res, next) => {
       message: "user Updated!",
     });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({
-        success: false,
-        message: "Server error",
-      });
+      success: false,
+      message: "Server error",
+    });
   }
 };
 
+//delete user
 export const deleteUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
 
-   
     await user.deleteOne();
 
     res.status(200).json({
       message: "Task Deleted!",
       success: true,
     });
-  } catch (error) {
-   
-  }
+  } catch (error) {}
 };
